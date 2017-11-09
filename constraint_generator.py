e751d3aaae346328eb3f76834356725f7839fe05
William Smith <williampsmith@berkeley.edu>
2017-11-08 23:46:26 -0800

    incomplete class interface for constraint generator

---
+++ constraint_generator.py
@@ -0,0 +1,9 @@
+class ConstraintGenerator:
+    """Enum constants for generator type"""
+    RANDOM = 0
+    SINGLE_SIDE_NEIGHBOR = 1
+
+    def ___init__(self, type=0):
+        """
+        """
+        self.type = type
