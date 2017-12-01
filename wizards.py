f31094f4a4cbc48890cb0e74a3a23b99fb22b4d5
Georgy Marrero <georgymarrero@gmail.com>
2017-11-30 18:02:57 -0800

    Clean a little

--- wizards.py
+++ wizards.py
@@ -19,9 +19,9 @@ class NonBetweenness(Annealer):
         shuffle(wizards)
         super(NonBetweenness, self).__init__(wizards)
         # set hyperparameters
-        self.Tmax = 500.0
-        self.Tmin = 0.5
-        self.steps = 1000000
+        self.Tmax = 1000000.0
+        self.Tmin = 0.8
+        self.steps = 50000
         self.updates = 1000
         # mapping for efficient position lookup by wizard name
         self.wiz_to_pos = {wizards[i] : i for i in range(len(wizards))}
@@ -35,9 +35,9 @@ class NonBetweenness(Annealer):
 
     def move(self):
         """Performs a move during the simmulated annealing algorithm."""
-        # self._move_satisfy_random_constraint()
+        self._move_satisfy_random_constraint()
         #self._move_randomly()
-        self._move_adjacently()
+        #self._move_adjacently()
 
     def _move_adjacently(self):
         a = randint(0, len(self.state) - 1)
@@ -59,7 +59,7 @@ class NonBetweenness(Annealer):
             if self._is_constraint_violated(c):
                 done = True
                 # swap 2 wizards to move closer
-                self._swap_wizards(c[1], c[2])
+                self._swap_wizards(c[random.randint(0, 1)], c[2])
                 # with probability 0.5, swap the two border wizards
                 # if random.randint(0, 1) == 1:
                 #     self._swap_wizards(c[0], c[1])
@@ -80,57 +80,3 @@ class NonBetweenness(Annealer):
             (self.wiz_to_pos[c[0]] < self.wiz_to_pos[c[2]] < self.wiz_to_pos[c[1]]) or
             (self.wiz_to_pos[c[1]] < self.wiz_to_pos[c[2]] < self.wiz_to_pos[c[0]])
         )
-
-# if __name__ == '__main__':
-#
-#     # latitude and longitude for the twenty largest U.S. cities
-#     cities = {
-#         'New York City': (40.72, 74.00),
-#         'Los Angeles': (34.05, 118.25),
-#         'Chicago': (41.88, 87.63),
-#         'Houston': (29.77, 95.38),
-#         'Phoenix': (33.45, 112.07),
-#         'Philadelphia': (39.95, 75.17),
-#         'San Antonio': (29.53, 98.47),
-#         'Dallas': (32.78, 96.80),
-#         'San Diego': (32.78, 117.15),
-#         'San Jose': (37.30, 121.87),
-#         'Detroit': (42.33, 83.05),
-#         'San Francisco': (37.78, 122.42),
-#         'Jacksonville': (30.32, 81.70),
-#         'Indianapolis': (39.78, 86.15),
-#         'Austin': (30.27, 97.77),
-#         'Columbus': (39.98, 82.98),
-#         'Fort Worth': (32.75, 97.33),
-#         'Charlotte': (35.23, 80.85),
-#         'Memphis': (35.12, 89.97),
-#         'Baltimore': (39.28, 76.62)
-#     }
-#
-#     # initial state, a randomly-ordered itinerary
-#     init_state = list(cities.keys())
-#     random.shuffle(init_state)
-#
-#     # create a distance matrix
-#     distance_matrix = {}
-#     for ka, va in cities.items():
-#         distance_matrix[ka] = {}
-#         for kb, vb in cities.items():
-#             if kb == ka:
-#                 distance_matrix[ka][kb] = 0.0
-#             else:
-#                 distance_matrix[ka][kb] = distance(va, vb)
-#
-#     tsp = TravellingSalesmanProblem(init_state, distance_matrix)
-#     tsp.steps = 100000
-#     # since our state is just a list, slice is the fastest way to copy
-#     tsp.copy_strategy = "slice"
-#     state, e = tsp.anneal()
-#
-#     while state[0] != 'New York City':
-#         state = state[1:] + state[:1]  # rotate NYC to start
-#
-#     print()
-#     print("%i mile route:" % e)
-#     for city in state:
-#         print("\t", city)
